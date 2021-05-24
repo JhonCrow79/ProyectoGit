08e52fb72e82957793e08b4349300142e6f3b605
LENOVO <LENOVO@DESKTOP-TTF2I6P>
2021-05-23 22:00:49 -0500

    Cambio 2

--- 1ProyectoGit/1ProyectoGit/src/pkg1proyectogit/Main.java
+++ 1ProyectoGit/1ProyectoGit/src/pkg1proyectogit/Main.java
@@ -18,6 +18,8 @@ public class Main {
         System.out.println("Hola mi nombre es Jhon Cuervo");
         
         System.out.println("Hola soy Camiloooo");
+        
+        System.out.println("PROGRAMACION II ");
     }
     
 }
