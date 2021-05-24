c20e972248b859ea943da161e3cf115fb109403d
JhonCrow79 <jhoncrow79@gmail.com>
2021-05-23 22:15:42 -0500

    4cambio

--- 1ProyectoGit/1ProyectoGit/src/pkg1proyectogit/Main.java
+++ 1ProyectoGit/1ProyectoGit/src/pkg1proyectogit/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mi nombre es Jhon");
+        System.out.println("Hola mi nombre es Jhon Cuervo");
         
         System.out.println("Hola soy Camiloooo");
         
