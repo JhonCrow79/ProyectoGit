930e9b95e6c767e6e0a74cc26a7d2ea40817a25e
LENOVO <LENOVO@DESKTOP-TTF2I6P>
2021-05-23 21:46:27 -0500

    Cambio 1

---
+++ src/pkg1proyectogit/Main.java
@@ -0,0 +1,18 @@
+
+package pkg1proyectogit;
+
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        System.out.println("PROGRAMACION II");
+        
+        System.out.println("Hola mi nombre es Jhon Cuervo");
+        
+        System.out.println("Hola soy Camiloooo");
+        
+    }
+    
+}
